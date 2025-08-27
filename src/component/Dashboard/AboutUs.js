9aa77f294e1a32367021cf78cc9910b07ea751e3
Jesu Khamrui <jesukhamrui@gmail.com>
2025-08-28 00:22:06 +0530

    Create AboutUs.js

---
+++ src/component/Dashboard/AboutUs.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './infoPages.css';
+export default function AboutUs() {
+  return (
+    <div className="info-page">
+      <h2>About Us</h2>
+      <p>Welcome to Tastivo! We are passionate about bringing delicious food from around the world to your table. Our mission is to make food ordering easy, fast, and enjoyable for everyone.</p>
+    </div>
+  );
+}
