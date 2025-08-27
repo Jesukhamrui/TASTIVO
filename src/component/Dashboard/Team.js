727a9ad850b1fccef3d4572a3fc05d579169545d
Jesu Khamrui <jesukhamrui@gmail.com>
2025-08-28 00:21:14 +0530

    Create Team.js

---
+++ src/component/Dashboard/Team.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './infoPages.css';
+export default function Team() {
+  return (
+    <div className="info-page">
+      <h2>Our Team</h2>
+      <p>Meet the Tastivo team! We are a group of food lovers, developers, and designers dedicated to making your food experience the best it can be.</p>
+    </div>
+  );
+}
