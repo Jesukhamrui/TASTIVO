f8dab64cc0c4afe0b284856d547702e9affab295
Jesu Khamrui <jesukhamrui@gmail.com>
2026-01-10 21:55:53 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartslice from "../component/Dashboard/cart/cartslice";
+import orderslice from "../component/Dashboard/orders/orderslice";
+import paymentslice from "./paymentslice";
+import reviewslice from "./reviewslice";
 
 export default configureStore({
     reducer:{
         cart:cartslice,
+        orders:orderslice,
+        payments:paymentslice,
+        reviews:reviewslice,
     }
 })
